9f752d3e314fe4bdf2d584fdba784a23dda0ae05
Eric Delmer Millen <ericdelmermillen@erics-mbp.home>
2024-05-29 13:55:53 -0400

    updateBio now working with deleting of prevBioImgURL on success and of new url on failure

--- controllers/bio-controller.js
+++ controllers/bio-controller.js
@@ -1,5 +1,6 @@
 const knex = require("knex")(require("../knexfile.js"));
 const { verifyToken } = require('../utils/utils.js');
+const { deleteFiles } = require("../s3.js");
 
 const AWS_BUCKET_PATH = process.env.AWS_BUCKET_PATH;
 
@@ -15,7 +16,6 @@ const getBio = async (req, res) => {
         bio_text: bioText 
       } = bioData;
 
-      // will need to send this concatenated with the aws bucket url and folder name
       const bioImgURL = `${AWS_BUCKET_PATH}bio/${bioData.bio_img_url}`;
       
       return res.json({
@@ -33,21 +33,88 @@ const getBio = async (req, res) => {
   }
 }
 
-const updateBio = async (req, res, next) => {
+// updateBio
+// add validation schema for bio data
+const updateBio = async (req, res) => {
   const token = req.headers.authorization; 
 
   // if(!verifyToken(token)) {
-  //   return res.status(401).send({message: "unauthorized"});
+  //   return res.status(401).send({ message: "unauthorized" });
   // }
 
-  try {
+  const { 
+    bio_name, 
+    bio_img_url, 
+    bio_text 
+  } = req.body;
 
-  } catch(error) {
-
+  // Validate request data
+  if (!bio_name || !bio_img_url || !bio_text) {
+    return res.status(400).json({ message: "Incomplete Bio update data sent" });
   }
+  
+  try {
+    // Get existing bio data
+    const bioData = await knex('bio').first(); 
+    const prevBioImgURL = bioData.bio_img_url;
 
-  res.json({message: "Bio page updated"});
+    // Update bio data
+    const updatedBio = await knex('bio')
+      .where('id', 1)
+      .update({
+        bio_name,
+        bio_text,
+        bio_img_url
+      });
+
+    if(updatedBio) {
+      // Fetch the updated bio data
+      const updatedBioData = await knex('bio').first(); 
+      
+      const {      
+        bio_name: updatedBioName,
+        bio_text: updatedBioText,
+        bio_img_url: updatedBioImgURL
+      } = updatedBioData;
+      
+      // Delete old AWS objects on successful update
+      try {
+        await deleteFiles([`bio/${prevBioImgURL}`]);
+      } catch (deleteError) {
+        console.error('Error deleting files from AWS:', deleteError);
+      }
+      
+      return res.json({
+        message: "Bio updated successfully",
+        bioName: updatedBioName,
+        bioText: updatedBioText,
+        bioImgURL: updatedBioImgURL
+      });
+    }
+
+  } catch (error) {
+    console.error('Error updating Bio page:', error);
+
+    // If updating bio fails, delete the AWS object added by the client
+    try {
+      console.log("Update failed: deleting new photo from AWS");
+      const deleteResponse = await deleteFiles([`bio/${bio_img_url}`]);
+
+      if(!deleteResponse) {
+        throw new Error("Error deleting files from AWS");
+      }
+
+    } catch(deleteError) {
+      // Handle errors in AWS object deletion
+      console.error("Error deleting file from AWS:", deleteError);
+      return res.status(500).send("Error deleting files from AWS");
+    }
+    
+    return res.status(500).send("Error updating Bio page");
+  }
 }
+
+
 
 module.exports = {
   getBio,
