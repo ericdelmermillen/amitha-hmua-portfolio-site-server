342ad0787639e2515d6c35ce688a937c3df2620c
Eric Delmer Millen <ericdelmermillen@erics-mbp.home>
2024-05-29 08:31:20 -0400

    added better error handling to getBio

--- controllers/bio-controller.js
+++ controllers/bio-controller.js
@@ -4,19 +4,31 @@ const knex = require("knex")(require("../knexfile.js"));
 // getBio to show bio page
 const getBio = async (req, res) => {
 
-  const bioData = await knex('bio').first(); 
+  try {
+    const bioData = await knex('bio').first(); 
 
-  const bioName = bioData.bio_name;
-  // will need to send this concatenated with the aws bucket url and folder name
-  const bioImgURL = bioData.bio_img_url;
-  
-  const bioText = bioData.bio_text;
-  
-  return res.json({
-    message: "Here's the bio", 
-    bioName, 
-    bioImgURL,
-    bioText});
+    if(bioData) {
+      const { 
+        bio_name: bioName, 
+        bio_text: bioText 
+      } = bioData;
+
+      // will need to send this concatenated with the aws bucket url and folder name
+      const bioImgURL = bioData.bio_img_url;
+      
+      return res.json({
+        bioName, 
+        bioImgURL,
+        bioText});
+    } else {
+      console.log("No bio data found in database");
+      return res.status(404).json({message: "Bio data not found"});
+    }
+
+  } catch(error) {
+    console.error("Error fetching bio data:", error);
+    return res.status(500).json({message: "An error occurred while fetching the Bio Page data"});
+  }
 }
 
 module.exports = {
