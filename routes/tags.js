d17a9a0089097a03a695c73a7d67e029ac4dfbed
Eric Delmer Millen <ericdelmermillen@erics-mbp.home>
2024-06-05 18:12:36 -0400

    removed all validation logic from tags route and replaced with validation via middleware

--- routes/tags.js
+++ routes/tags.js
@@ -2,7 +2,7 @@ const tagsController = require('../controllers/tags-controller');
 const tagsRouter = require('express').Router();
 const { validationResult } = require('express-validator');
 const { paramsIsNumber, tagDataValid } = require('../utils/validationSchemas.js');
-const { validateToken } = require("./middleware/middleware.js");
+const { validateToken, validateRequest } = require("./middleware/middleware.js");
 
 
 // get models route
@@ -12,45 +12,17 @@ tagsRouter.route('/all')
 
 // // add model route  
 tagsRouter.route('/add')
-  .post(validateToken, tagDataValid, (req, res, next) => {
-    const errors = validationResult(req);
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      return res.status(400).json({ errors: errorMsgs });
-    }
-
-    next();
-  }, tagsController.addTag);
+  .post(validateToken, validateRequest(tagDataValid), tagsController.addTag);
   
   
 // edit tag by id route  
 tagsRouter.route('/edit/:id')
-  .put(validateToken, paramsIsNumber, tagDataValid, (req, res, next) => {
-    const errors = validationResult(req);
-
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      console.log(errors)
-      return res.status(400).json({ errors: errorMsgs });
-    }
-    next();
-  }, tagsController.editTagById);
+  .put(validateToken, validateRequest(paramsIsNumber, tagDataValid), tagsController.editTagById);
 
   
 // // delete model route
 tagsRouter.route('/delete/:id')
-  .delete(validateToken, paramsIsNumber, (req, res, next) => {
-    const errors = validationResult(req);
-
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      return res.status(400).json({ errors: errorMsgs });
-    }
-    next();
-  }, tagsController.deleteTagByID);
+  .delete(validateToken, validateRequest(paramsIsNumber), tagsController.deleteTagByID);
 
 
 module.exports = tagsRouter;
