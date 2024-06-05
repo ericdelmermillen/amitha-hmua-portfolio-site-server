ebe389a252eca4a04f3fc7a84a14330e74c239e5
Eric Delmer Millen <ericdelmermillen@erics-mbp.home>
2024-06-05 17:59:47 -0400

    replaced validation in models route with validation via middleware

--- routes/models.js
+++ routes/models.js
@@ -1,8 +1,8 @@
 const modelsController = require('../controllers/models-controller.js');
 const modelsRouter = require('express').Router();
-const { validationResult } = require('express-validator');
 const { paramsIsNumber, modelDataValid } = require('../utils/validationSchemas.js');
 const { validateToken } = require("./middleware/middleware.js");
+const { validateRequest } = require("./middleware/middleware.js");
 
 
 // get models route
@@ -12,46 +12,17 @@ modelsRouter.route('/all')
 
 // add model route  
 modelsRouter.route('/add')
-  .post(validateToken, modelDataValid, (req, res, next) => {
-    const errors = validationResult(req);
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      return res.status(400).json({ errors: errorMsgs });
-    }
-    
-    next();
-  }, modelsController.addModel);
+  .post(validateToken, validateRequest(modelDataValid), modelsController.addModel);
   
   
 // edit model by id route  
 modelsRouter.route('/edit/:id')
-  .put(validateToken, paramsIsNumber, modelDataValid, (req, res, next) => {
-    const errors = validationResult(req);
-
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      return res.status(400).json({ errors: errorMsgs });
-    }
-
-    next();
-  }, modelsController.editModelById);
+  .put(validateToken, validateRequest(paramsIsNumber, modelDataValid), modelsController.editModelById);
 
   
 // delete model route
 modelsRouter.route('/delete/:id')
-  .delete(validateToken, paramsIsNumber, (req, res, next) => {
-    const errors = validationResult(req);
-
-    const errorMsgs = errors.array().map(error => error.msg);
-
-    if(!errors.isEmpty()) {
-      return res.status(400).json({ errors: errorMsgs });
-    }
-    
-    next();
-  }, modelsController.deleteModelByID);
+  .delete(validateToken, validateRequest(paramsIsNumber), modelsController.deleteModelByID);
 
 
 module.exports = modelsRouter;
