779f84fceb7b2d0d1a5334beba64da9ed3d913ea
Eric Millen <ericdelmermillen@gmail.com>
2025-07-05 13:01:28 -0400

    model functionailty now working affter refactor to use moudle syntax

--- routes/models.mjs
+++ routes/models.mjs
@@ -5,12 +5,19 @@ import { validateToken, validateRequest } from '../middleware/middleware.mjs';
 
 const modelsRouter = express.Router();
 
-modelsRouter.route('/')
+modelsRouter.route('/all')
   .get(getAllModels)
+
+modelsRouter.route('/add')
   .post(validateToken, validateRequest(modelDataValid), addModel);
 
 modelsRouter.route('/:id')
   .put(validateToken, validateRequest(paramsIsNumber, modelDataValid), editModelById)
+
+modelsRouter.route('/delete/:id')
   .delete(validateToken, validateRequest(paramsIsNumber), deleteModelByID);
+
+modelsRouter.route('/edit/:id')
+  .put(validateToken, validateRequest(paramsIsNumber, modelDataValid), editModelById)
 
 export default modelsRouter;
